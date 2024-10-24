4d3f31102fa74557d763190f34402a3679f7a0ff
idragoi <john.dragoi87@gmail.com>
2024-10-24 14:39:50 +0300

    test reusable action

--- aws_accounts/app_backend/input.tf
+++ aws_accounts/app_backend/input.tf
@@ -1,3 +1,8 @@
+variable "cloud_env" {
+  type = string
+  description = "Value representing the Landing Zone environment. Will be set by the pipeline as TF_VAR_cloud_env based on the repo branch."
+}
+
 ########################################################################################################################
 # Variables loaded from JSON file
 ########################################################################################################################
@@ -5,8 +10,7 @@
 locals {
   global_config = jsondecode(file("${path.root}/../../config/global_config.json"))
   module_config = jsondecode(file("${path.root}/../../config/module_config.json"))
-  environment = local.module_config.env_variables.environment
   account_name = basename(abspath(path.root))
-  account_id = local.global_config.org_structure[local.environment].accounts[local.account_name].id
-  account_config = local.module_config.accounts[local.account_name]
+  account_id = local.global_config.org_structure[var.cloud_env].accounts[local.account_name].id
+  account_config = local.module_config[var.cloud_env].accounts[local.account_name]
 }
--- aws_accounts/operations/tf_backend.tf
+++ aws_accounts/operations/tf_backend.tf
@@ -1,9 +1,10 @@
 terraform {
   backend "s3" {
-    bucket = "{{ TF_STATE_S3_BUCKET }}"
-    key    = "platform/{{ ENVIRONMENT }}/{{ ACCOUNT_NAME }}/{{ MODULE }}"
+    bucket = "{{ TF_VAR_tf_state_s3_bucket }}"
+    key    = "platform/{{ TF_VAR_cloud_env }}/{{ account_name }}/{{ TF_VAR_module }}"
+    region = "{{ TF_VAR_default_region }}"
     assume_role = {
-      role_arn = "{{ TF_STATE_IAM_ROLE_ARN }}"
+      role_arn = "{{ TF_VAR_tf_state_iam_role_arn }}"
     }
   }
 }

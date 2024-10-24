3bdf5d01c0a87b530ddb352f8fedac86a84ab9f7
idragoi <john.dragoi87@gmail.com>
2024-10-24 16:06:36 +0300

    test reusable action

--- aws_accounts/operations/tf_backend.tf
+++ aws_accounts/operations/tf_backend.tf
@@ -1,10 +1,10 @@
 terraform {
   backend "s3" {
-    bucket = "{{ TF_VAR_tf_state_s3_bucket }}"
-    key    = "platform/{{ TF_VAR_cloud_env }}/{{ account_name }}/{{ TF_VAR_module }}"
-    region = "{{ TF_VAR_default_region }}"
+    bucket = "__TF_VAR_tf_state_s3_bucket__"
+    key    = "platform/__TF_VAR_cloud_env__/__account_name__/__TF_VAR_module__"
+    region = "__TF_VAR_default_region__"
     assume_role = {
-      role_arn = "{{ TF_VAR_tf_state_iam_role_arn }}"
+      role_arn = "__TF_VAR_tf_state_iam_role_arn__"
     }
   }
 }

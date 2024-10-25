6591f7be410effd80f5b08cfc4fefe31cbbeff9e
idragoi <john.dragoi87@gmail.com>
2024-10-25 15:49:57 +0300

    test reusable action

--- terraform/tf_config.tf
+++ terraform/tf_config.tf
@@ -1,38 +1,40 @@
-# terraform {
-#   required_providers {
-#     aws = {
-#       source = "hashicorp/aws"
-#       version = "__TF_VAR_aws_provider_version__"
-#     }
-#     github = {
-#       source  = "integrations/github"
-#       version = "__TF_VAR_github_provider_version__"
-#     }
-#   }
-# }
-
-# provider "github" {}
-
-# terraform {
-#   backend "s3" {
-#     bucket = "__TF_VAR_tf_state_s3_bucket__"
-#     key    = "platform/__TF_VAR_cloud_env__/__TF_VAR_module__"
-#     region = "__TF_VAR_default_region__"
-#     assume_role = {
-#       role_arn = "__TF_VAR_tf_state_iam_role_arn__"
-#     }
-#   }
-# }
-
 terraform {
+  required_version = "__TF_VAR_terraform_version__"
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "__TF_VAR_aws_provider_version__"
     }
     github = {
       source  = "integrations/github"
-      version = "~> 6.0"
+      version = "__TF_VAR_github_provider_version__"
     }
   }
 }
+
+provider "github" {}
+
+terraform {
+  backend "s3" {
+    bucket = "__TF_VAR_tf_state_s3_bucket__"
+    key    = "platform/__TF_VAR_cloud_env__/__TF_VAR_module__"
+    region = "__TF_VAR_default_region__"
+    assume_role = {
+      role_arn = "__TF_VAR_tf_state_iam_role_arn__"
+    }
+  }
+}
+
+# terraform {
+  # required_version = ">= 1.9"
+#   required_providers {
+#     aws = {
+#       source = "hashicorp/aws"
+#       version = "~> 5.0"
+#     }
+#     github = {
+#       source  = "integrations/github"
+#       version = "~> 6.0"
+#     }
+#   }
+# }

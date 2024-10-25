f4ebf94cc7ac4b3ca99d6f95a221e1d6411cb9c7
idragoi <john.dragoi87@gmail.com>
2024-10-25 11:13:58 +0300

    test reusable action

--- aws_accounts/operations/main.tf
+++ aws_accounts/operations/main.tf
@@ -4,6 +4,6 @@
 resource "aws_vpc" "this" {
     cidr_block = local.account_config.vpc_name
     tags = {
-        Name = "${local.global_config.naming_prefix}${local.account_config.vpc_name}"
+        Name = "${var.global_config.naming_prefix}${local.account_config.vpc_name}"
     }
 }

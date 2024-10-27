ec7341d7c87d76d963535be53ad27389a680021d
idragoi <john.dragoi87@gmail.com>
2024-10-27 21:58:21 +0200

    create vpcs

--- aws_accounts/operations/main.tf
+++ aws_accounts/operations/main.tf
@@ -1,9 +1,9 @@
 ########################################################################################################################
 # VPC creation
 ########################################################################################################################
-# resource "aws_vpc" "this" {
-#     cidr_block = local.account_config.primary_cidr_block
-#     tags = {
-#         Name = "${var.global_config.naming_prefix}${local.account_config.vpc_name}"
-#     }
-# }
+resource "aws_vpc" "this" {
+    cidr_block = local.account_config.primary_cidr_block
+    tags = {
+        Name = "${var.global_config.naming_prefix}${local.account_config.vpc_name}"
+    }
+}
